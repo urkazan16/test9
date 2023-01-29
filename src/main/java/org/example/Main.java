f0bc606148a4079be39428e730cc42476fb9f419
user <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>
2023-01-29 14:56:04 +0300

    test update files 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,10 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world! 777");
     }
 }
