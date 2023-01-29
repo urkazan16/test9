03072e400958afe5bd135aa41f8ce4d6d3d63151
user <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>
2023-01-29 14:58:27 +0300

    test update files 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,5 +12,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world! 777");
+        System.out.println("Hello world! 777");
+
+
     }
 }
