b9e7622e96fadd0d1a1f575c2693ba2aebda974d
user <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>
2023-01-29 14:59:58 +0300

    test update files 4 and delete files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world! 777");
-        System.out.println("Hello world! 777");
 
 
     }
